2a0c65eb595e662c878a730606b45becbcba2fbc
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-05-05 05:26:49 +0000

    finish3

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,5 +9,19 @@ class Book < ApplicationRecord
   end
 
 	validates :title, presence: true
-	validates :body, presence: true, length: {maximum: 200}
+	
+	# 検索方法分岐
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @book = Book.where("title LIKE?","#{word}")
+    elsif search == "forward_match"
+      @book = Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("title LIKE?","%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about'=> 'homes#about'
+  get "search" => "search#search"
 
   post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
   post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
